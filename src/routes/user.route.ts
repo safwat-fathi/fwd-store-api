758c09f2c017c5bf98d141eee1803be1a5e4e46b
Safwat Fathi <safwat.fathi@jobzella.com>
2022-11-15 18:56:47 +0200

    add: verifyToken to show user

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { login, signup, show } from "../controllers/user.controllers";
+import { verifyToken } from "../middlewares/auth.middlewares";
 
 const users = Router();
 
 users.post("/login", login);
 users.post("/signup", signup);
-users.get("/:userId", show);
+users.get("/:userId", verifyToken, show);
 
 export default users;
